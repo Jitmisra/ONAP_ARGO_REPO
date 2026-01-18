2e1a00bb0c9e5ee25091652c1ce57dff459cdc13
Jitmisra <agnikmisra@gmail.com>
2026-01-18 23:09:10 +0530

    Configure OOM ArgoCD for HPE15

--- argo/updateVariables.sh
+++ argo/updateVariables.sh
@@ -6,11 +6,11 @@ DIRS=("argocd" "infra" "onap" "onap-test")
 # Variables and Replacements (Key=Variable, Value=Replacement)
 # Beispiel: VAR1="Wert1", VAR2="Wert2"
 declare -A VARS
-VARS["ONAP_ARGO_REPO_URL"]="https://git.onap.org/oom"
-VARS["ONAP_ARGO_BRANCH"]="master"
-VARS["STORAGECLASS"]="cinder-os"
-VARS["BASEURL"]="simpledemo.onap.org"
-VARS["POSTADDR"]="-test"
+VARS["ONAP_ARGO_REPO_URL"]="https://github.com/Jitmisra/ONAP_ARGO_REPO"
+VARS["ONAP_ARGO_BRANCH"]="main"
+VARS["STORAGECLASS"]="storage-local-static-provisioner"
+VARS["BASEURL"]="hpe15.anuket.iol.unh.edu"
+VARS["POSTADDR"]=""
 VARS["DOCKER_REPO"]="docker.io"
 VARS["ONAP_REPO"]="nexus3.onap.org:10001"
 VARS["ELASTIC_REPO"]="docker.elastic.co"
